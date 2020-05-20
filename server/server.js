1bbc5175dd08461ec853810a63393a3ee0379660
mta630 <michael.t.albert89@gmail.com>
2020-05-20 18:33:57 -0500

    Added CRUD endpoints

--- server/server.js
+++ server/server.js
@@ -17,6 +17,36 @@ app.get('/carousel/:gameId', (req, res) => {
   });
 });
 
+app.delete('/carousel/:gameId', (req, res) => {
+  db.deleteGameForTopCarousel(req.params.gameId, (err, result) => {
+    if (err) {
+      res.status(404).end();
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+app.put('/carousel/:gameId', (req, res) => {
+  db.editGameForTopCarousel(req.params.gameId, (err, result) => {
+    if (err) {
+      res.status(404).end();
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+app.post('/carousel/:gameId', (req, res) => {
+  db.addGameForTopCarousel(req.params.gameId, (err, result) => {
+    if (err) {
+      res.status(404).end();
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening at port:${PORT}`);
 });
